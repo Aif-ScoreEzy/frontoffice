2106fba1c801aef70f5781a1b3881f1b10d4eee8
ariefaiforesee <arief@aiforesee.com>
2023-04-10 21:24:30 +0700

    change middleware validator

--- config/routes.go
+++ config/routes.go
@@ -12,18 +12,18 @@ import (
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 
-	api.Post("/role", middleware.Auth(), middleware.IsRoleRequestValid, role.CreateRole)
+	api.Post("/role", middleware.Auth(), middleware.IsRequestValid(role.RoleRequest{}), role.CreateRole)
 	api.Get("/roles", middleware.Auth(), role.GetAllRoles)
 	api.Get("/role/:id", middleware.Auth(), role.GetRoleByID)
-	api.Put("/role/:id", middleware.Auth(), middleware.IsRoleRequestValid, role.UpdateRole)
+	api.Put("/role/:id", middleware.Auth(), middleware.IsRequestValid(role.RoleRequest{}), role.UpdateRole)
 	api.Delete("/role/:id", middleware.Auth(), role.DeleteRole)
 
-	api.Post("/permission", middleware.Auth(), middleware.IsPermissionRequestValid, permission.CreatePermission)
+	api.Post("/permission", middleware.Auth(), middleware.IsRequestValid(permission.PermissionRequest{}), permission.CreatePermission)
 	api.Get("/permission/:id", middleware.Auth(), permission.GetRoleByID)
-	api.Put("/permission/:id", middleware.Auth(), middleware.IsPermissionRequestValid, permission.UpdatePermissionByID)
+	api.Put("/permission/:id", middleware.Auth(), middleware.IsRequestValid(permission.PermissionRequest{}), permission.UpdatePermissionByID)
 	api.Delete("/permission/:id", middleware.Auth(), permission.DeletePermissionByID)
 
-	api.Post("/register", middleware.IsRegisterUserRequestValid, user.Register)
-	api.Post("/login", middleware.IsLoginRequestValid, user.Login)
-	api.Put("/user/:id", middleware.Auth(), middleware.IsUpdateUserRequestValid, user.UpdateUserByID)
+	api.Post("/register", middleware.IsRequestValid(user.RegisterUserRequest{}), user.Register)
+	api.Post("/login", middleware.IsRequestValid(user.UserLoginRequest{}), user.Login)
+	api.Put("/user/:id", middleware.Auth(), middleware.IsRequestValid(user.UpdateUserRequest{}), user.UpdateUserByID)
 }
--- middleware/validation.go
+++ middleware/validation.go
@@ -2,105 +2,30 @@ package middleware
 
 import (
 	"front-office/helper"
-	"front-office/pkg/permission"
-	"front-office/pkg/role"
-	"front-office/pkg/user"
+	"reflect"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/usepzaka/validator"
 )
 
-func IsRoleRequestValid(c *fiber.Ctx) error {
-	request := &role.RoleRequest{}
-	if err := c.BodyParser(&request); err != nil {
-		resp := helper.ResponseFailed(err.Error())
+func IsRequestValid(model interface{}) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		request := reflect.New(reflect.TypeOf(model)).Interface()
 
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
+		if err := c.BodyParser(request); err != nil {
+			resp := helper.ResponseFailed(err.Error())
+
+			return c.Status(fiber.StatusBadRequest).JSON(resp)
+		}
+
+		if errValid := validator.ValidateStruct(request); errValid != nil {
+			resp := helper.ResponseFailed(errValid.Error())
+
+			return c.Status(fiber.StatusBadRequest).JSON(resp)
+		}
+
+		c.Locals("request", request)
+
+		return c.Next()
 	}
-
-	if errValid := validator.ValidateStruct(request); errValid != nil {
-		resp := helper.ResponseFailed(errValid.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	c.Locals("request", request)
-
-	return c.Next()
-}
-
-func IsPermissionRequestValid(c *fiber.Ctx) error {
-	request := &permission.PermissionRequest{}
-	if err := c.BodyParser(&request); err != nil {
-		resp := helper.ResponseFailed(err.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	if errValid := validator.ValidateStruct(request); errValid != nil {
-		resp := helper.ResponseFailed(errValid.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	c.Locals("request", request)
-
-	return c.Next()
-}
-
-func IsRegisterUserRequestValid(c *fiber.Ctx) error {
-	request := &user.RegisterUserRequest{}
-	if err := c.BodyParser(&request); err != nil {
-		resp := helper.ResponseFailed(err.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	if errValid := validator.ValidateStruct(request); errValid != nil {
-		resp := helper.ResponseFailed(errValid.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	c.Locals("request", request)
-
-	return c.Next()
-}
-
-func IsLoginRequestValid(c *fiber.Ctx) error {
-	request := &user.UserLoginRequest{}
-	if err := c.BodyParser(&request); err != nil {
-		resp := helper.ResponseFailed(err.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	if errValid := validator.ValidateStruct(request); errValid != nil {
-		resp := helper.ResponseFailed(errValid.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	c.Locals("request", request)
-
-	return c.Next()
-}
-
-func IsUpdateUserRequestValid(c *fiber.Ctx) error {
-	request := &user.UpdateUserRequest{}
-	if err := c.BodyParser(&request); err != nil {
-		resp := helper.ResponseFailed(err.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	if errValid := validator.ValidateStruct(request); errValid != nil {
-		resp := helper.ResponseFailed(errValid.Error())
-
-		return c.Status(fiber.StatusBadRequest).JSON(resp)
-	}
-
-	c.Locals("request", request)
-
-	return c.Next()
 }
