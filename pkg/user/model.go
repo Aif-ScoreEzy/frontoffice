0170b6aee51933d1e43d6ad1bbe8405d3bae37ff
ariefaiforesee <arief@aiforesee.com>
2023-04-12 08:37:35 +0700

    change user model

--- pkg/user/model.go
+++ pkg/user/model.go
@@ -10,18 +10,18 @@ import (
 )
 
 type User struct {
-	ID        string          `json:"id"`
-	Name      string          `json:"name"`
-	Username  string          `json:"username" gorm:"unique"`
-	Email     string          `json:"email" gorm:"unique"`
-	Password  string          `json:"password"`
-	Phone     string          `json:"phone"`
-	Key       string          `json:"key"`
+	ID        string          `gorm:"primarykey" json:"id"`
+	Name      string          `gorm:"not null" json:"name"`
+	Username  string          `gorm:"unique" json:"username"`
+	Email     string          `gorm:"unique" json:"email"`
+	Password  string          `gorm:"not null" json:"password"`
+	Phone     string          `gorm:"not null" json:"phone"`
+	Key       string          `gorm:"not null" json:"key"`
 	Active    bool            `json:"active"`
-	CompanyID string          `json:"company_id"`
-	Company   company.Company `json:"company" gorm:"foreignKey:CompanyID"`
-	RoleID    string          `json:"role_id"`
-	Role      role.Role       `json:"role" gorm:"foreignKey:RoleID"`
+	CompanyID string          `gorm:"not null" json:"company_id"`
+	Company   company.Company `gorm:"foreignKey:CompanyID" json:"company"`
+	RoleID    string          `gorm:"not null" json:"role_id"`
+	Role      role.Role       `gorm:"foreignKey:RoleID" json:"role"`
 	CreatedAt time.Time       `json:"-"`
 	UpdatedAt time.Time       `json:"-"`
 	DeletedAt gorm.DeletedAt  `gorm:"index" json:"-"`
@@ -29,9 +29,9 @@ type User struct {
 
 type RegisterUserRequest struct {
 	Name            string `json:"name" validate:"required~Name cannot be empty"`
-	Email           string `json:"email" gorm:"unique" validate:"required~Email cannot be empty, email~Only email pattern are allowed"`
+	Email           string `json:"email" validate:"required~Email cannot be empty, email~Only email pattern are allowed"`
 	Password        string `json:"password" validate:"required~Password cannot be empty, length(8)~Password must have at least 8 characters"`
-	Username        string `json:"username" gorm:"unique" validate:"required~Username cannot be empty, alphanum~Only alphabet and numeric values are allowed for username"`
+	Username        string `json:"username" validate:"required~Username cannot be empty, alphanum~Only alphabet and numeric values are allowed for username"`
 	Phone           string `string:"phone" validate:"required~Phone cannot be empty, phone"`
 	CompanyName     string `json:"company_name"`
 	CompanyAddress  string `json:"company_address"`
@@ -45,16 +45,16 @@ type RegisterUserRequest struct {
 type RegisterUserResponse struct {
 	ID        string          `json:"id"`
 	Name      string          `json:"name"`
-	Username  string          `json:"username" gorm:"unique"`
-	Email     string          `json:"email" gorm:"unique"`
+	Username  string          `json:"username"`
+	Email     string          `json:"email"`
 	Password  string          `json:"-"`
 	Phone     string          `json:"phone"`
 	Key       string          `json:"key"`
 	Active    bool            `json:"active"`
 	CompanyID string          `json:"-"`
-	Company   company.Company `json:"company" gorm:"foreignKey:CompanyID"`
+	Company   company.Company `json:"company"`
 	RoleID    string          `json:"-"`
-	Role      role.Role       `json:"role" gorm:"foreignKey:RoleID"`
+	Role      role.Role       `json:"role"`
 	CreatedAt time.Time       `json:"-"`
 	UpdatedAt time.Time       `json:"-"`
 	DeletedAt gorm.DeletedAt  `gorm:"index" json:"-"`
@@ -76,8 +76,8 @@ type UserLoginResponse struct {
 
 type UpdateUserRequest struct {
 	Name      string `json:"name"`
-	Username  string `json:"username" gorm:"unique" validate:"alphanum~Only alphabet and numeric values are allowed for username"`
-	Email     string `json:"email" gorm:"unique" validate:"email~Only email pattern are allowed"`
+	Username  string `json:"username" validate:"alphanum~Only alphabet and numeric values are allowed for username"`
+	Email     string `json:"email" validate:"email~Only email pattern are allowed"`
 	Phone     string `string:"phone" validate:"phone"`
 	RoleID    string `json:"role_id"`
 	CompanyID string `json:"company_id"`
