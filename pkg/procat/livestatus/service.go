06d393d89770e41b7fc2f72559fa5e865bb1c7d4
ariefro <ariefromadhon26@gmail.com>
2024-04-20 08:13:07 +0700

    add service to reprocess unsuccessful job detail

--- pkg/procat/livestatus/service.go
+++ pkg/procat/livestatus/service.go
@@ -3,30 +3,34 @@ package livestatus
 import (
 	"encoding/json"
 	"fmt"
+	"front-office/app/config"
 	"io"
 	"strconv"
 )
 
-func NewService(repo Repository) Service {
-	return &service{Repo: repo}
+func NewService(cfg *config.Config, repo Repository) Service {
+	return &service{Cfg: cfg, Repo: repo}
 }
 
 type service struct {
+	Cfg  *config.Config
 	Repo Repository
 }
 
 type Service interface {
 	CreateJob(data []LiveStatusRequest, totalData int) (uint, error)
 	GetJobs(page, limit string) ([]*Job, error)
+	GetJobByID(jobID uint) (*Job, error)
 	GetJobsTotal() (int64, error)
 	GetJobDetails(jobID uint) ([]*JobDetail, error)
 	GetJobDetailsWithPagination(page, limit, keyword string, jobID uint) ([]*JobDetail, error)
 	GetJobDetailsWithPaginationTotal(keyword string, jobID uint) (int64, error)
 	GetJobDetailsPercentage(column, keyword string, jobID uint) (int64, error)
-	ProcessBatchJobDetails(apiKey string, jobID uint, batch []*JobDetail) ([]*LiveStatusResponse, error)
-	ProcessJobDetails(apiKey string, jobID uint, jobDetails []*JobDetail, batchSize int) ([]*LiveStatusResponse, error)
+	GetUnprocessedJobDetails() ([]*JobDetail, error)
+	ProcessJobDetails(jobDetail *JobDetail, successRequestTotal int) (int, error)
 	CreateLiveStatus(liveStatusRequest *LiveStatusRequest, apiKey string) (*LiveStatusResponse, error)
 	UpdateJob(id uint, total int) error
+	UpdateProcessedJobDetail(id uint) error
 	UpdateSucceededJobDetail(id uint, subcriberStatus, deviceStatus string) error
 	UpdateFailedJobDetail(id uint, sequence int) error
 	DeleteJobDetail(id uint) error
@@ -52,6 +56,10 @@ func (svc *service) GetJobs(page, limit string) ([]*Job, error) {
 	offset := (intPage - 1) * intLimit
 
 	return svc.Repo.GetJobs(intLimit, offset)
+}
+
+func (svc *service) GetJobByID(jobID uint) (*Job, error) {
+	return svc.Repo.GetJobByID(jobID)
 }
 
 func (svc *service) GetJobsTotal() (int64, error) {
@@ -91,55 +99,51 @@ func (svc *service) GetJobDetailsPercentage(column, keyword string, jobID uint) 
 	return count, err
 }
 
-func (svc *service) ProcessBatchJobDetails(apiKey string, jobID uint, batch []*JobDetail) ([]*LiveStatusResponse, error) {
-	var liveStatusResponse *LiveStatusResponse
-	var liveStatusResponses []*LiveStatusResponse
-	// var err error
-	jobIDStr := strconv.FormatUint(uint64(jobID), 10)
-
-	for _, jobDetail := range batch {
-		fmt.Printf("Processing Job Detail ID: %d\n", jobDetail.ID)
-		request := &LiveStatusRequest{
-			PhoneNumber: jobDetail.PhoneNumber,
-			TrxID:       jobIDStr,
-		}
-
-		liveStatusResponse, _ = svc.CreateLiveStatus(request, apiKey)
-		// if err != nil {
-		// 	return nil, err
-		// }
-
-		liveStatusResponses = append(liveStatusResponses, liveStatusResponse)
-
-		_ = svc.DeleteJobDetail(jobDetail.ID)
-		// if err != nil {
-		// 	return nil, err
-		// }
+func (svc *service) GetUnprocessedJobDetails() ([]*JobDetail, error) {
+	jobDetails, err := svc.Repo.GetUnprocessedJobDetails()
+	if err != nil {
+		return nil, err
 	}
 
-	return liveStatusResponses, nil
+	return jobDetails, nil
 }
 
-func (svc *service) ProcessJobDetails(apiKey string, jobID uint, jobDetails []*JobDetail, batchSize int) ([]*LiveStatusResponse, error) {
-	var liveStatusResponses []*LiveStatusResponse
-	var err error
-	numJobDetails := len(jobDetails)
-	for i := 0; i < numJobDetails; i += batchSize {
-		end := i + batchSize
-		if end > numJobDetails {
-			end = numJobDetails
-		}
+func (svc *service) ProcessJobDetails(jobDetail *JobDetail, successRequestTotal int) (int, error) {
+	apiKey := svc.Cfg.Env.ApiKeyLiveStatus
+	jobIDStr := strconv.FormatUint(uint64(jobDetail.JobID), 10)
+	request := &LiveStatusRequest{
+		PhoneNumber: jobDetail.PhoneNumber,
+		TrxID:       jobIDStr,
+	}
 
-		batch := jobDetails[i:end]
+	liveStatusResponse, err := svc.CreateLiveStatus(request, apiKey)
+	if err != nil {
+		return 0, err
+	}
 
-		fmt.Printf("Processing batch %d to %d\n", i, end)
-		liveStatusResponses, err = svc.ProcessBatchJobDetails(apiKey, jobID, batch)
+	// todo: jika status code 200 kirim job detail ke aifcore
+
+	dataMap := liveStatusResponse.Data.(map[string]interface{})
+	dataLiveMap := dataMap["live"].(map[string]interface{})
+	subscriberStatus := fmt.Sprintf("%v", dataLiveMap["subscriber_status"])
+	deviceStatus := fmt.Sprintf("%v", dataLiveMap["device_status"])
+
+	// todo: jika status code 200 maka hapus job detail pada temp tabel. Sampai aifcore menyediakan API untuk get job details, untuk sementara jika status code 200 lakukan update subcriber_status dan device_status pada job detail
+	if liveStatusResponse.StatusCode == 200 {
+		successRequestTotal += 1
+		// err = svc.DeleteJobDetail(jobDetail.ID)
+		err = svc.UpdateSucceededJobDetail(jobDetail.ID, subscriberStatus, deviceStatus)
 		if err != nil {
-			return nil, err
+			return 0, err
+		}
+	} else {
+		_ = svc.UpdateFailedJobDetail(jobDetail.JobID, jobDetail.Sequence)
+		if err != nil {
+			return 0, err
 		}
 	}
 
-	return liveStatusResponses, nil
+	return successRequestTotal, nil
 }
 
 func (svc *service) CreateLiveStatus(liveStatusRequest *LiveStatusRequest, apiKey string) (*LiveStatusResponse, error) {
@@ -162,13 +166,21 @@ func (svc *service) UpdateJob(id uint, total int) error {
 	return svc.Repo.UpdateJob(id, total)
 }
 
+func (svc *service) UpdateProcessedJobDetail(id uint) error {
+	request := &UpdateJobDetailRequest{
+		OnProcess: true,
+	}
+
+	return svc.Repo.UpdateJobDetail(id, request)
+}
+
 func (svc *service) UpdateSucceededJobDetail(id uint, subcriberStatus, deviceStatus string) error {
 	request := &UpdateJobDetailRequest{
 		SubscriberStatus: subcriberStatus,
 		DeviceStatus:     deviceStatus,
 	}
 
-	return svc.Repo.UpdateSucceededJobDetail(id, request)
+	return svc.Repo.UpdateJobDetail(id, request)
 }
 
 func (svc *service) UpdateFailedJobDetail(jobID uint, sequence int) error {
@@ -177,7 +189,7 @@ func (svc *service) UpdateFailedJobDetail(jobID uint, sequence int) error {
 		Sequence:  sequence + 1,
 	}
 
-	return svc.Repo.UpdateFailedJobDetail(jobID, request)
+	return svc.Repo.UpdateJobDetail(jobID, request)
 }
 
 func (svc *service) DeleteJobDetail(id uint) error {
