afb7f10dfc28ec6d07fac007e6eb63857b76c55f
ariefro <ariefromadhon26@gmail.com>
2024-07-15 15:47:09 +0700

    add MstCompany model

--- pkg/core/company/model.go
+++ pkg/core/company/model.go
@@ -22,6 +22,23 @@ type Company struct {
 	DeletedAt       gorm.DeletedAt    `gorm:"index" json:"-"`
 }
 
+type MstCompany struct {
+	CompanyID       uint    `json:"company_id" gorm:"primaryKey;autoIncrement"`
+	CompanyName     string  `json:"company_name"`
+	CompanyAddress  string  `json:"company_address"`
+	CompanyPhone    string  `json:"company_phone"`
+	AgreementNumber string  `json:"agreement_number"`
+	PaymentScheme   string  `json:"payment_scheme"`
+	PostpaidActive  bool    `json:"active"`
+	IndustryId      uint    `json:"industry_id"`
+	BasePricing     float64 `json:"base_pricing"`
+	// Apiconfigs      []apiconfig.MstApiconfig `json:"apiconfigs" gorm:"foreignKey:CompanyId"`
+	// Products        []MstSubscribedProduct   `json:"products" gorm:"foreignKey:CompanyId"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
+
 type UpdateCompanyRequest struct {
 	CompanyName     string `json:"company_name"`
 	CompanyAddress  string `json:"company_address"`
