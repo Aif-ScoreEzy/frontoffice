e600960ec285380f94c4900a1f4bef70ccf51df8
ariefaiforesee <arief@aiforesee.com>
2025-04-15 15:53:32 +0700

    fix service get log operation

--- common/constant/events.go
+++ common/constant/events.go
@@ -1,14 +1,17 @@
 package constant
 
 const (
-	EventSignIn               = "sign in"
-	EventSignOut              = "sign out"
-	EventChangePassword       = "change password"
-	EventRequestPasswordReset = "request password reset"
-	EventPasswordReset        = "password reset"
-	EventRegisterMember       = "register member"
-	EventUpdateProfile        = "update profile"
-	EventUpdateUserData       = "update user data"
-	EventCalculateScore       = "calculate score"
-	EventDownloadScoreHistory = "download score history"
+	EventSignIn                    = "sign in"
+	EventSignOut                   = "sign out"
+	EventChangePassword            = "change password"
+	EventRequestPasswordReset      = "request password reset"
+	EventPasswordReset             = "password reset"
+	EventRegisterMember            = "register member"
+	EventUpdateProfile             = "update profile"
+	EventUpdateUserData            = "update user data"
+	EventCalculateScore            = "calculate score"
+	EventDownloadScoreHistory      = "download score history"
+	EventChangeBillingInformation  = "change billing information"
+	EventTopupBalance              = "topup balance"
+	EventSubmitPaymentConfirmation = "sumbmit payment confirmation"
 )
