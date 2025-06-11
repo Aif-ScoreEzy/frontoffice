ff26688534e3c81efef29b3671e050c2f3986f54
ariefaiforesee <arief@aiforesee.com>
2025-06-11 08:54:26 +0700

    restructure compliance endpoints

--- pkg/procat/loanrecordchecker/init.go
+++ pkg/procat/loanrecordchecker/init.go
@@ -14,5 +14,5 @@ func SetupInit(apiGroup fiber.Router, cfg *config.Config, client httpclient.HTTP
 	controller := NewController(service)
 
 	loanRecordCheckerGroup := apiGroup.Group("loan-record-checker")
-	loanRecordCheckerGroup.Post("/", middleware.Auth(), middleware.IsRequestValid(LoanRecordCheckerRequest{}), middleware.GetJWTPayloadFromCookie(), controller.LoanRecordChecker)
+	loanRecordCheckerGroup.Post("/single-request", middleware.Auth(), middleware.IsRequestValid(LoanRecordCheckerRequest{}), middleware.GetJWTPayloadFromCookie(), controller.LoanRecordChecker)
 }
