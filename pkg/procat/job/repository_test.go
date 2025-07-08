32b4b5260877e445f3167256362c5600e12d763e
ariefaiforesee <arief@aiforesee.com>
2025-07-08 11:25:47 +0700

    add unit tests for fetch procat job

--- pkg/procat/job/repository_test.go
+++ pkg/procat/job/repository_test.go
@@ -193,3 +193,64 @@ func TestCallUpdateJobAPI(t *testing.T) {
 		mockClient.AssertExpectations(t)
 	})
 }
+
+func TestCallGetProCatJobAPI(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		mockData := model.AifcoreAPIResponse[any]{
+			Success: true,
+		}
+		body, err := json.Marshal(mockData)
+		require.NoError(t, err)
+
+		resp := &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(bytes.NewReader(body)),
+		}
+
+		repo, mockClient := setupMockRepo(t, resp, nil)
+
+		result, err := repo.CallGetProCatJobAPI(&logFilter{})
+
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		mockClient.AssertExpectations(t)
+	})
+
+	t.Run("NewRequestError", func(t *testing.T) {
+		mockClient := new(MockClient)
+		repo := NewRepository(&config.Config{
+			Env: &config.Environment{AifcoreHost: constant.MockInvalidHost},
+		}, mockClient, nil)
+
+		_, err := repo.CallGetProCatJobAPI(&logFilter{})
+
+		assert.Error(t, err)
+	})
+
+	t.Run("HTTPRequestError", func(t *testing.T) {
+		expectedErr := errors.New(constant.ErrHTTPReqFailed)
+
+		repo, mockClient := setupMockRepo(t, nil, expectedErr)
+
+		_, err := repo.CallGetProCatJobAPI(&logFilter{})
+
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), constant.ErrHTTPReqFailed)
+		mockClient.AssertExpectations(t)
+	})
+
+	t.Run("ParseError", func(t *testing.T) {
+		resp := &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader(`{invalid-json`)),
+		}
+
+		repo, mockClient := setupMockRepo(t, resp, nil)
+
+		result, err := repo.CallGetProCatJobAPI(&logFilter{})
+
+		assert.Nil(t, result)
+		assert.Error(t, err)
+		mockClient.AssertExpectations(t)
+	})
+}
