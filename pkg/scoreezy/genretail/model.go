dda089c22964e151586db7f96219766ab3a6ea96
ariefaiforesee <arief@aiforesee.com>
2025-08-11 18:41:10 +0700

    add model log trans

--- pkg/scoreezy/genretail/model.go
+++ pkg/scoreezy/genretail/model.go
@@ -43,10 +43,30 @@ type GenRetailV3ClientReturnSuccess struct {
 	Data    *dataGenRetailV3 `json:"data"`
 }
 
-type GenRetailV3ClientReturnError struct {
-	Message      string           `json:"message"`
-	ErrorMessage string           `json:"error_message"`
-	Data         *dataGenRetailV3 `json:"data"`
+type logTransScoreezy struct {
+	LogTrxId             uint                  `json:"log_trx_id" gorm:"primaryKey;autoIncrement"`
+	TrxId                string                `json:"trx_id"`
+	MemberId             uint                  `json:"user_id"`
+	Member               member.MstMember      `json:"user"`
+	CompanyId            uint                  `json:"company_id"`
+	IpClient             string                `json:"ip_client"`
+	ProductId            uint                  `json:"product_id"`
+	Status               string                `json:"status"`  // Free or Pay
+	Success              bool                  `json:"success"` // true or false
+	Message              string                `json:"message"`
+	ProbabilityToDefault string                `json:"probability_to_default"`
+	Grade                string                `json:"grade"`
+	LoanNo               string                `json:"loan_no"`
+	Data                 *dataLogTransScoreezy `json:"data" swaggertype:"object"`
+	Duration             time.Duration         `json:"duration" format:"duration" example:"2h30m"`
+	CreatedAt            time.Time             `json:"created_at" format:"date-time"`
+}
+
+type dataLogTransScoreezy struct {
+	Name        string `json:"name"`
+	PhoneNumber string `json:"phone_no"`
+	IdCardNo    string `json:"id_card_no"`
+	LoanNo      string `json:"loan_no"`
 }
 
 type UploadScoringRequest struct {
